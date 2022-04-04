ed9882f70905ce042dad0dd680a70644f7b6b803
EngrThOYe <adetoy_4real@yahoo.com>
2022-04-04 23:19:59 +0100

    first commit

--- resoure.tf
+++ resoure.tf
@@ -1,10 +1,47 @@
-resource "local_file" "pet"{
-    filename = "C:/Users/EngrThOYe/Desktop/Newfolder/test.txt"
-    content = var.headline
+resource "local_file" "pet" {
+  filename = "C:/Users/EngrThOYe/Desktop/Newfolder/test.txt"
+  //content = "hello world"
+  //content = var.headline_list[0]
+  //content = var.headline
+  //content = var.headline_map["statement1"]
+  // variable will be ssupply durring terraform plan 
+  content = var.headline_empty
+  // using the output of the resources random pet as our content
+  //content = "my pet name is ${random_pet.my-pet.id}"
+
+
 }
 
-resource "random_pet" "my-pet"{
-    prefix = "Mrs"
-    separator = "."
-    length = "1"
+resource "random_pet" "my-pet" {
+  prefix    = "Mrs"
+  separator = "."
+  length    = "1"
 }
+
+output "pet_name" {
+  value       = random_pet.my-pet.id
+  description = "random name generated in the resource pet name"
+
+}
+
+resource "local_file" "cat" {
+  filename = "C:/Users/EngrThOYe/Desktop/Newfolder/cat.txt"
+  content  = "hello world"
+
+  lifecycle {
+    //this prevent resoures been destroye before creation (inmutable resurses)
+    //creat_before_destroy = true
+    //prevent resources from bn deleted in error e.g databases
+    //prevent_destroy = true
+    //prrevent resources from modifications like EC2
+    //ignore_changes = [
+    //   tags,aim
+    //]
+    //ignore_changes = all
+  }
+}
+
+data "local_file" "dog" {
+  filename = "C:/Users/EngrThOYe/Desktop/Newfolder/dog.txt"
+}
+    
--- variables.tf
+++ variables.tf
@@ -1,3 +1,58 @@
 variable "headline" {
-    default = "testing if varible will use the text"
+  default = "testing if varible will use the text"
+  //optional if not specified type = any, else type = sting, number, bool, list ["abc", "xyz"], map {"a1" = "abc", "a2" = "xyz"), object, tuple
+  type        = string
+  description = "content of the file"
+}
+
+variable "headline_map" {
+  type = map(any)
+  default = {
+    "statement1" = "Testing Map statement 1"
+    "statement2" = "Testing Map statement 2"
+    "statement3" = "Testing Map statement 3"
+  }
+}
+
+variable "headline_list" {
+  type    = list(any)
+  default = ["Testing list statement 1", "Testing list statement 2", "Testing list statement 3", "Testing list statement 1"]
+
+}
+
+variable "headline_set" {
+  type = set(string)
+  //we cant have dupplicate valuee in set unlike list
+  default = ["Testing set statement 1", "Testing set statement 2", "Testing set statement 3", "Testing set statement 4"]
+}
+
+variable "headline_list_string" {
+  type    = list(string)
+  default = ["Testing list statement 1", "Testing list statement 2", "Testing list statement 3"]
+}
+
+variable "headline_object" {
+  type = object({
+    name     = string
+    color    = string
+    age      = number
+    chritain = bool
+  })
+  default = {
+    name     = "my name"
+    color    = "my color"
+    age      = 22
+    chritain = true
+  }
+}
+
+variable "headline_tuple" {
+  type    = tuple([string, number, bool])
+  default = ["Testing list statement 1", 2, false]
+
+}
+
+//variable block can be empty and be provided when running terraform apply
+variable "headline_empty" {
+
 }
